0d057fb2bacd209beb12f8610b78f5677af936b8
Priyam1464 <priyam.chhabra704@gmail.com>
2018-02-26 00:40:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article1',function(req,res)
+app.get('/ui/article1.html',function(req,res)
 {
      res.send('Article 1');
 });
