41c71679ff08ddc89435e5cd418049603ea7f25f
Priyam1464 <priyam.chhabra704@gmail.com>
2018-02-26 19:56:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 console.log('Loaded!');
 var counter=0;
+var button=document.getElementById('butt');
 var element=document.getElementById('me');
-element.onClick(function()
+button.onClick(function()
 {
    counter+=1;
    element.innerHTML(counter.toString());
