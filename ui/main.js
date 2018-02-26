2de927803e652cf368b1806d98569a01eefdf345
Priyam1464 <priyam.chhabra704@gmail.com>
2018-02-26 20:01:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@
 var counter=0;
 var button=document.getElementById('butt');
 var element=document.getElementById('me');
-button.onClick(function()
+button.onClick=function()
 {
-   counter+=1;
-   element.innerHTML(counter.toString());
-});
+   counter=counter+1;
+   element.innerHTML=counter.toString();
+};
