dd2a7851c23a271fba5219f9566ea32b128f3e4f
Priyam1464 <priyam.chhabra704@gmail.com>
2018-02-26 19:02:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,14 +4,46 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var article1={
+    title:"article1",
+    date:"24th February 2018",
+     content: `<p>
+     There's a fire in your eyes
+     </p>
+      <p>
+      A lifetime of memories
+      </p>`,
+    author:"Priyam's lifeline"
+};
+function createTemplate(data)
+{
+    var title=data.title;
+    var date=data.date;
+    var item=data.content;
+    var htmlTemplate=`
+    <html>
+<head>
+    <title>${title}</title>
+    <link rel="stylesheet" href="ui/style.css">
+</head>    
+    <body>
+   <h1 href="/"><a href="/">Home</a></h1>
+   <hr/>
+   <div class="content">
+      <h3>${date}</h3>
+      ${item}
+   </div>
+    </body>
+</html> `;
+    return htmlTemplate;  
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article1',function(req,res)
 {
-      res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
+      res.send(createTemplate(article1));
 });
 app.get('/article2',function(req,res)
 {
